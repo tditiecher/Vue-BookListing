247fa097265a25151a6a457052d2891af1bc851d
Taco D <taco@tdev.org>
2020-11-29 11:40:34 +0100

    Step 3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="book in books" :key="book.title + book.author">
+        {{ book.title }}:{{ book.author }}
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,6 +15,11 @@ export default {
   data() {
     return {
       title: "All Books",
+      books: [
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson" },
+        { title: "American Gods", author: "Neil Gaiman" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman" },
+      ],
     };
   },
 };
@@ -19,5 +29,14 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
